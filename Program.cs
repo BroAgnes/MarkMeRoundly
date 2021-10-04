31bb83ea6f0676955c777c1c0ce5f0b79d6473e8
agnese.broka <agnese.broka@LVAGPLTP3270>
2021-10-04 14:39:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace gradingTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("C:/Users/agnese.broka/source/repos/gardingTask/input.txt");
+            int[] grades = text.Split(',').Select(int.Parse).ToArray();
+            Console.WriteLine(String.Join(", ", grades));
+            for(int i = 0; i < grades.Length; i++)
+            {
+                if (grades[i] < 38) continue;
+                int gradeLeft = (grades[i] / 10) * 10;
+                int gradeRight = grades[i] % 10;
+                int gradeRoundToNumber = gradeRight > 5 ? gradeLeft + 10 : gradeLeft + 5;
+                int roundedGrade = gradeRoundToNumber - grades[i] >= 3 ? grades[i] : gradeRoundToNumber;
+                grades[i] = roundedGrade;
+            }
+
+            File.WriteAllText("output.txt", String.Join(", ", grades));
+            text = System.IO.File.ReadAllText("output.txt");
+            Console.WriteLine(text);
+
+        }
+    }
+}
